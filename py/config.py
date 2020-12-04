c1e4385bf775fe4c1a2b3e42618949b03049713b
Karan Botadra <kbot983@gmail.com>
2020-12-03 20:34:11 -0500

    Initial commit

---
+++ py/config.py
@@ -0,0 +1,16 @@
+# IP to attack
+dst_ip = "ubuntu-server"
+# Number of IPs
+n_ips = "50"
+# Number of messages per IP
+n_msg = "100000"
+# Interface
+interface = "docker0"
+# Type
+# "1" = Flood; "2"=Teardrop; "3"=Black nurse
+type = "2"
+# IPs used
+# "1" = From ips.txt; "2" = Random IPs
+orig_type = "2"
+#Threads
+threads= "3"
